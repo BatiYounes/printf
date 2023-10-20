56fc647d5de3a8f344f252c948e2a18afdb35b11
evil-ghost <danielaisosajoseph@outlook.com>
2023-10-19 20:08:34 -0700

    Add struct for identifiers call

--- test/main.h
+++ test/main.h
@@ -6,4 +6,15 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int _check_spec(char, va_list);
 
+/**
+ * struct idn - identifiers and right func call
+ * @i: first member
+ * @f: second member
+ */
+typedef struct idn
+{
+	char i;
+	int (*f)(va_list);
+} idn;
+
 #endif /* MAIN_H */
