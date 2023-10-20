578533566b8b6751773ca9a9d00e70abcc37658b
evil-ghost <danielaisosajoseph@outlook.com>
2023-10-19 17:38:38 -0700

    Create _putchar.c

---
+++ test/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints character on the stdout
+ * @c: character argument
+ *
+ * Return: returns 1(success) otherwise -1 on error
+ */
+int _putchar(char *c)
+{
+	return (write(1, &c, 1));
+}
