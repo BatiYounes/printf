cdc4075a47d0d79a027544e824632345f4a0d89d
evil-ghost <danielaisosajoseph@outlook.com>
2023-10-20 21:49:52 -0700

    Create print_letters.c

---
+++ test/print_letters.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * print_chr - prints the character on the stdout
+ * @args: variadic argument
+ *
+ * Return: returns number of characters printed
+ */
+int print_chr(va_list args)
+{
+	char c;
+
+	c = va_arg(args, int);
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * print_str - prints the strings on the stdout
+ * @args: variadic argument
+ *
+ * Return: returns number of characters printed
+ */
+int print_str(va_list args)
+{
+	char *str;
+	int i;
+
+	str = va_arg(args, char *);
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+
+	return (i);
+}
