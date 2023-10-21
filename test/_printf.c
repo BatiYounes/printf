a5c0a578c2501d97c52e503aa3c16c7933564de7
evil-ghost <danielaisosajoseph@outlook.com>
2023-10-20 21:52:02 -0700

    Create _printf.c

---
+++ test/_printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * _printf - a function for writing output to the stdout
+ * @format: an array of characters
+ *
+ * Return: returns the total number of printed char
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count, operation;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	count = 0;
+	while (*(format))
+	{
+		if (*(format) == '%')
+		{
+			operation = check_spec(*(format + 1), args);
+			if (operation > 0)
+			{
+				format += 2;
+				count += operation;
+			}
+
+			if (operation == 0)
+			{
+				_putchar(*(format++));
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(*(format++));
+			count++;
+		}
+	}
+
+	va_end(args);
+	return (count);
+}
